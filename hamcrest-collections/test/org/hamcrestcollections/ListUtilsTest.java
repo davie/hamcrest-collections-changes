7bba0ccd2853c4c226a8e05b73b58c4f395147a1
davie moston <daviemoston@gmail.com>
2008-02-25 22:38:48 +0000

    davie: whitespace

--- hamcrest-collections/test/org/hamcrestcollections/ListUtilsTest.java
+++ hamcrest-collections/test/org/hamcrestcollections/ListUtilsTest.java
@@ -1,7 +1,8 @@
 package org.hamcrestcollections;
 
-import junit.framework.TestCase;
-import org.junit.Test;import static org.junit.Assert.assertEquals;import static org.junit.Assert.fail;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 import java.util.Collections;
 import java.util.Arrays;
