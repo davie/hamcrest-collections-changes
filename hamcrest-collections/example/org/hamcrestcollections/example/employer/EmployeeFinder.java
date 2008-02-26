10438042490c9fe52a76066fb12895cb264aa8aa
davie moston <daviemoston@gmail.com>
2008-02-26 00:14:48 +0000

    davie: bastardise EmployeeFinder - use hasproperty to work around generics

--- hamcrest-collections/example/org/hamcrestcollections/example/employer/EmployeeFinder.java
+++ hamcrest-collections/example/org/hamcrestcollections/example/employer/EmployeeFinder.java
@@ -3,22 +3,59 @@ package org.hamcrestcollections.example.employer;
 import static org.hamcrestcollections.Selector.select;
 import org.hamcrestcollections.Function;
 import org.hamcrestcollections.FunctionMapper;
-import org.hamcrest.beans.HasPropertyWithValue;import static org.hamcrest.Matchers.equalTo;
+import org.hamcrest.beans.HasPropertyWithValue;
+import static org.hamcrest.Matchers.equalTo;
+
+import java.util.List;
+import java.util.ArrayList;
 
 public class EmployeeFinder {
-    private static Function<Employee, Object> stdoutEmployeePrinter = new Function<Employee, Object>() {
-        public Object apply(Employee employee) {
-            System.out.println("employee = " + employee);
+    private List<Object> employees;
+
+    public static void main(String[] args) {
+
+        List<Object> myEmployees = new ArrayList<Object>();
+
+        myEmployees.addAll(EmployeeRecords.ALL);
+        myEmployees.add(new Me());
+        myEmployees.add(new You(28));
+
+        Iterable<Object> chosenEmployees = new EmployeeFinder(myEmployees).findWithAge(28);
+        FunctionMapper.map(chosenEmployees, stdoutEmployeePrinter);
+    }
+
+    private Iterable<Object> findWithAge(Integer idealEmployeeAge) {
+        return select(employees, HasPropertyWithValue.hasProperty("age", equalTo(idealEmployeeAge)));
+    }
+
+    private static Function<Object, Object> stdoutEmployeePrinter = new Function<Object, Object>() {
+        public Object apply(Object employee) {
+            System.out.println("thing = " + employee);
             return null;
         }
     };
 
-    public static void main(String[] args) {
-        Iterable<Employee> employees = new EmployeeFinder().findWithAge(28);
-        FunctionMapper.map(employees, stdoutEmployeePrinter);
+    public EmployeeFinder(List<Object> MY_EMPLOYEES) {
+        this.employees = MY_EMPLOYEES;
     }
 
-    private Iterable<Employee> findWithAge(Integer idealEmployeeAge) {
-        return select(EmployeeRecords.ALL, HasPropertyWithValue.<Employee>hasProperty("age", equalTo(idealEmployeeAge)));
+
+    private static class Me {}
+
+    public static class You {
+        private You(int age) {
+            this.age = age;
+        }
+
+        public String toString() {
+            return "You can't hide, your age is: " + age;
+        }
+
+        private final int age;
+
+        public int getAge() {
+            return age;
+
+        }
     }
 }
